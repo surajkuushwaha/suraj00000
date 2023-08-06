29008827222a9d36c1f59c51789079fd7ba96303
Suraj Kushwaha <suraj.kushwaha@culturex.in>
2023-08-06 15:07:27 +0530

    FEAT : experience section is done

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import { Navbar } from "./Pages/Navbar/Navbar";
 import { Hero } from "./Pages/Hero/Hero";
 import { Experience } from "./Pages/Exprience/Experience";
+
 function App() {
   return (
     <>
       {/* <Routes> */}
       <Navbar />
-        <Hero />
-        <Experience />
+      <Hero />
+      <Experience />
       {/* </Routes> */}
     </>
   );
--- src/Pages/Exprience/Experience.tsx
+++ src/Pages/Exprience/Experience.tsx
@@ -1,27 +1,27 @@
 import React from "react";
 import "./experience.css";
+import { Line } from "../../Components/Line/Line";
 
-const exprience = [
+const experience = [
   {
-    title: "Software Developer (Remote)",
-    date: "Jun 2021 - Present",
-    company: "Selfbook",
-    location: " Selfbook / US - New York",
+    title: "Full Stack Software Developer (MERN)",
+    date: "july 2022 - Present",
+    company: "CultureX",
+    location: "Ahmedabad - Gujarat",
     description: [
-      "Developing screens and UI components for the web application using React and Tailwind.",
-      "Fixing UI issues and integrating backend APIs with Redux Saga.",
+      "Build and design a multi step form in react for generating leads.",
+      "Help to integrate multiple social media platform on a single application",
+      "Created a NPM icon package to be used by team members",
     ],
   },
   {
-    title: "Software Developer",
-    date: "Jun 2022 - Present",
-    company: "CultureX",
+    title: "Summer Internship in React",
+    date: "June 2022 - July 2022",
+    company: "INFOLABZ",
     location: " Ahmedabad - Gujarat",
     description: [
-      "Developing screens and UI components for the web application using React and Tailwind.",
-      "Fixing UI issues and integrating backend APIs with Redux Saga.",
-      "Fixing UI issues and integrating backend APIs with Redux Saga.",
-      "Fixing UI issues and integrating backend APIs with Redux Saga.",
+      "Learn about the basic of ReactJS Framework",
+      "Improve my javascript skills and logic building.",
     ],
   },
   {
@@ -41,14 +41,14 @@ const exprience = [
 interface CompanyCardProps {
   company: string;
   selected?: boolean;
-  onClick?: (index: number) => void;
+  onClick?: () => void;
 }
 
 const CompanyCard = ({ company, selected, onClick }: CompanyCardProps) => {
   return (
     <div
-      className={`company-card ${selected && "selected"}`}
-      onClick={() => onClick?.(0)}
+      className={`company-card ${selected && "selected"} text-primary`}
+      onClick={() => onClick?.()}
     >
       <div className="company-card-title">{company}</div>
     </div>
@@ -61,25 +61,42 @@ export const Experience = () => {
   const handleSelect = (index: number): void | undefined => {
     setSelected(index);
   };
-  console.log(selected);
   return (
-    <div className="experience">
-      <a href="#experience" id="experience" className="heading">
-        experience
-      </a>
-      <div className="main">
-        <div className="right">
-          {exprience.map((item, index) => (
-            <CompanyCard
-              key={index}
-              company={item.company}
-              onClick={() => handleSelect(index)} // Pass the function reference
-              selected={index === selected}
-            />
-          ))}
+    <>
+      <div className="experience">
+        <a href="#experience" id="experience" className="heading text-primary">
+          experience
+        </a>
+        <div className="main">
+          <div className="right">
+            {experience.map((item, index) => (
+              <CompanyCard
+                key={index}
+                company={item.company}
+                onClick={() => handleSelect(index)} // Pass the function reference
+                selected={index === selected}
+              />
+            ))}
+          </div>
+          <div className="left">
+            <div className="company-description text-primary">
+              <div className="company-heading">
+                <div className="heading-title">
+                  {experience[selected].title}
+                </div>
+                <div className="heading-date">{experience[selected].date}</div>
+              </div>
+              <div className="location">{experience[selected].location}</div>
+              {experience[selected].description.map((item, index) => (
+                <div key={index} className="description-item">
+                  - {item}
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
-        <div className="left"></div>
+        <Line />
       </div>
-    </div>
+    </>
   );
 };
